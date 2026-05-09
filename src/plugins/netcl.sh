441bc1a79b85528b45eddd74a83ab60a943b1c9e
noobzhax <azorax@pm.me>
2026-05-09 12:13:49 +0700

    fix(shell): Fix quoting, comparisons, and grep
    Replace string comparison operators (> <) with numeric (-gt -lt) in
    process count checks. Fix grep patterns to prevent self-matching using
    [p]hp-cli bracket trick. Quote variables and printf format strings for
    safety. Consolidate repeated curl calls into single response variable.

--- src/plugins/netcl.sh
+++ src/plugins/netcl.sh
@@ -2,9 +2,9 @@
 echo -e "Current DHCP leases of connected devices:<code>\n"
 file='/tmp/dhcp.leases'
 while IFS= read -r line <&3; do
-  echo -e "Device      : $(printf "$line" | awk '{print$4}')"
-  echo -e "Mac Address : $(printf "$line" | awk '{print$2}')"
-  echo -e "IP Address  : $(printf "$line" | awk '{print$3}')"
+  echo -e "Device      : $(printf "%s" "$line" | awk '{print$4}')"
+  echo -e "Mac Address : $(printf "%s" "$line" | awk '{print$2}')"
+  echo -e "IP Address  : $(printf "%s" "$line" | awk '{print$3}')"
   echo -e "Expired     : $(date -d @$(printf '%s' "$line" | awk '{print $1}') +'%y/%m/%d %H:%M')"
   echo -e "\n"
 done 3< $file
--- src/plugins/stats.sh
+++ src/plugins/stats.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 rpcaria=$(uci get aria2.main._rpc_url)
 
-stopped=$(curl "$rpcaria" -H "Content-Type: application/json" -H "Accept: application/json" --data '{"jsonrpc": "2.0","id":"foo", "method": "aria2.getGlobalStat", "params":[]}' | cut -d "{" -f 3 | cut -d "," -f 3 | cut -d ":" -f 2 |tr -d '"')
+RESPONSE=$(curl -s "$rpcaria" -H "Content-Type: application/json" -H "Accept: application/json" --data '{"jsonrpc": "2.0","id":"foo", "method": "aria2.getGlobalStat", "params":[]}')
 
-
-active=$(curl "$rpcaria" -H "Content-Type: application/json" -H "Accept: application/json" --data '{"jsonrpc": "2.0","id":"foo", "method": "aria2.getGlobalStat", "params":[]}' | cut -d "{" -f 3 | cut -d "," -f 2 | cut -d ":" -f 2 |tr -d '"')
-
-
-waiting=$(curl "$rpcaria" -H "Content-Type: application/json" -H "Accept: application/json" --data '{"jsonrpc": "2.0","id":"foo", "method": "aria2.getGlobalStat", "params":[]}' | cut -d "{" -f 3 | cut -d "," -f 5 | cut -d ":" -f 2 |tr -d '"')
-
-
-dspeed=$(curl "$rpcaria" -H "Content-Type: application/json" -H "Accept: application/json" --data '{"jsonrpc": "2.0","id":"foo", "method": "aria2.getGlobalStat", "params":[]}' | cut -d "{" -f 3 | cut -d "," -f 1 | cut -d ":" -f 2 |tr -d '"')
+active=$(echo "$RESPONSE" | cut -d "{" -f 3 | cut -d "," -f 2 | cut -d ":" -f 2 | tr -d '"')
+stopped=$(echo "$RESPONSE" | cut -d "{" -f 3 | cut -d "," -f 3 | cut -d ":" -f 2 | tr -d '"')
+waiting=$(echo "$RESPONSE" | cut -d "{" -f 3 | cut -d "," -f 5 | cut -d ":" -f 2 | tr -d '"')
+dspeed=$(echo "$RESPONSE" | cut -d "{" -f 3 | cut -d "," -f 1 | cut -d ":" -f 2 | tr -d '"')
 dspeedkb=$(printf '%.0f ' "$((dspeed / 1024))")
 
 echo "XWRT - Aria2 Control Panel"
